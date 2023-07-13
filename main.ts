de6fffebc0a9e566b19001e352ed26f453ac9c86
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-13 15:33:17 -0700

    created based routes

--- main.ts
+++ main.ts
@@ -1,27 +1,8 @@
-import Hapi from "@hapi/hapi";
-
-
-const init = async () => {
-  const server = new Hapi.Server({
-    port: 3000,
-    host: "localhost",
-  });
-
-  server.route({
-    method: "POST",
-    path: "/users",
-    handler: (request, h) => {
-      return "Hello World!";
-    },
-  });
-
-  await server.start();
-  console.log("Server running on", server.info.uri);
-};
+import root from "./routes/root";
 
 process.on("unhandledRejection", (err) => {
   console.error(err);
   process.exit(1);
 });
 
-init();
+root();
