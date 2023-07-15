2d9a03f8ca3c323380c30da806a684489df33a06
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-07-14 17:11:18 -0700

    skip and limit

--- routes/offers/handlers/getOffer.ts
+++ routes/offers/handlers/getOffer.ts
@@ -1,15 +1,12 @@
 import Hapi from "@hapi/hapi";
 import { GimWorkResponse } from "../../types";
-import { Offer } from "@prisma/client";
 import cognitoAuth from "../../utils/cognitoAuth";
-import extractEmailFromCognito from "../../utils/getEmailFromCognitoResponse";
 import pickCategory from "../../utils/pickCategory";
 
 async function getOffer(
   request: Hapi.Request<Hapi.ReqRefDefaults>,
   h: Hapi.ResponseToolkit<Hapi.ReqRefDefaults>
 ) {
-  const payload = request.payload as Offer;
   let GimWorkResponse: GimWorkResponse<unknown>;
 
   try {
@@ -17,11 +14,24 @@ async function getOffer(
     const token = authorizationHeader?.split(" ")[1];
     await cognitoAuth(token!);
 
-    const {minPrice, maxPrice, category, minDate, maxDate, userId } = request.params;
+    const {minPrice, maxPrice, category, minDate, maxDate, userId, limit, skip } = request.params;
     const _minPrice = minPrice ? parseInt(minPrice, 10) : 0;
     const _maxPrice = maxPrice ? parseInt(maxPrice, 10) : 1000000;
     const _minDate = minDate ? new Date(minDate) : new Date("2021-01-01T00:00:00.000Z");
     const _maxDate = maxDate ? new Date(maxDate) : new Date("3000-12-31T00:00:00.000Z");
+    const _limit = limit ? parseInt(limit, 10) : 10;
+    if (_limit > 100) {
+      throw new Error("Limit cannot be greater than 100");
+    }
+    let _skip;
+    if (skip) {
+      const totalOffers = await request.server.app.prisma.offer.count();
+      const parsedSkip = parseInt(skip, 10);
+      if (parsedSkip > totalOffers) {
+        throw new Error("Skip cannot be greater than total offers");
+      }
+      _skip = parsedSkip;
+    }
 
     const offers = await request.server.app.prisma.offer.findMany({
       where: {
@@ -40,6 +50,8 @@ async function getOffer(
           userId: userId ? userId : undefined,
         },
       },
+      take: _limit,
+      skip: _skip,
     });
 
     GimWorkResponse = {
